cc11e56954b8688857149194a9aa42c18d06c273
Pirillo, Chris J <piric001@ww-am02036090.wdw.disney.com>
2015-12-27 17:41:15 -0500

    Fixes animations on hover properties. Abstracted to class for further animation.

---
+++ js/portfolio-hover.js
@@ -0,0 +1,9 @@
+$(function() {
+
+ $('.portfolio-circle').hover(function(){
+    $('.behance-quickview').addClass('slide');
+  }, function(){
+    $('.behance-quickview').removeClass('slide');
+  });
+
+});
